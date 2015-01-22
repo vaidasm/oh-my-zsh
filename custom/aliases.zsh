54303feb770ff21ae5db5389af5a648ed99e1f14
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2015-01-22 15:06:03 +0200

    add vim wrapper and some aliases

---
+++ custom/aliases.zsh
@@ -0,0 +1,14 @@
+# Wrappers
+alias vim='~/.oh-my-zsh/bin/vim.wrapper'
+
+# ArchLinux aliases
+alias pacman='sudo pacman'
+alias systemctl='sudo systemctl'
+alias service='sudo systemctl'
+
+
+# Dirs
+alias sites=~/Sites
+alias www=~/Sites
+alias ind=~/Sites/individuum/api
+alias ego=~/Sites/egopay
